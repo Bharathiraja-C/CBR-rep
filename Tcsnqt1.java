79655e1875419e1c67f5b88b6d04d70c53ea0328
Bharathiraja C <117998792+Bharathiraja-C@users.noreply.github.com>
2023-08-14 14:11:09 +0530

    Update Tcsnqt1.java

--- Tcsnqt1.java
+++ Tcsnqt1.java
@@ -1,28 +1,29 @@
 import  java.util.*;
 public class Tcsnqt1{
 	public static void main(String args[]){
-		int k=15,q=0,l=0;
+		int k=2,q=0,l=0;
 		List<Integer> b=new ArrayList<Integer>();
-		String s="100100001";
+		String s="10111101100";
 		char[] a=s.toCharArray();
 		for(char c:a){
 			if(c=='1') {
-				q++;;
-				if(l<=k){
-					q=q+l;
-					b.add(q);
-				}
-				l=0;
-				q=1;
+				q++;
+				
 			}
 			else{
-				l++;
+			    l++;
 				if(l>k){
 					b.add(q+k);
+					l=0;
 					q=0;
 					
 				}
-				
+				else if(l==k+1){
+					q=q+l;
+					b.add(q);
+					l=0;
+				    q=0;
+				}
 			}
 		}
 		for(Integer i:b){
